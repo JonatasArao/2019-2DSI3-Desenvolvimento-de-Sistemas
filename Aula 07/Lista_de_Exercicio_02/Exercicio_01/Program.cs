9717038b8bbdfa0d8e8c44b78e411fd7cd272bb3
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2019-08-15 20:27:07 -0300

    Aula 07 - Revisão Array e Vetores

---
+++ Aula 07/Lista_de_Exercicio_02/Exercicio_01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[4];
+
+            numeros = colocaNumero(numeros);
+
+            mostraVetor(numeros);
+
+            numeros = multiplicaNumero(numeros);
+
+            mostraVetor(numeros);
+        }
+        static int[] colocaNumero(int[] retornaNumero)
+        {
+            int razao;
+
+            Console.Clear();
+            Console.Write("Escolha a razão dos números do vetor: ");
+            razao = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i<retornaNumero.Length; i++)
+            {
+                retornaNumero[i] = i * razao;
+            }
+
+            return retornaNumero;
+        }
+        static void mostraVetor(int[] vetor)
+        {
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+            Console.Write("Clique para continuar");
+            Console.ReadKey();
+        }
+        static int[] multiplicaNumero(int[] retornaNumero)
+        {
+            int multiplicador;
+
+            Console.Clear();
+            Console.Write("Escolha o multiplicador dos números do vetor: ");
+            multiplicador = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < retornaNumero.Length; i++)
+            {
+                retornaNumero[i] *= multiplicador;
+            }
+
+            return retornaNumero;
+        }
+    }
+}
---
+++ Aula 07/Lista_de_Exercicio_02/Exercicio_02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numeros = new int[3, 4];
+
+            numeros = botColocaNumero(numeros);
+
+            mostraMediaMatriz(numeros);
+
+            Console.ReadKey();
+        }
+        static int[,] botColocaNumero(int[,] retornaNumero)
+        {
+            int razao = 4, cont = 0;
+            for (int i = 0; i < retornaNumero.GetLength(0); i++)
+            {
+                for (int j = 0; j < retornaNumero.GetLength(1); j++)
+                {
+                    cont++;
+                    retornaNumero[i, j] = cont * razao;
+                }
+            }
+
+            return retornaNumero;
+        }
+        static void mostraMediaMatriz(int[,] numero)
+        {
+            int media = somaValoresMatriz(numero) / numero.Length;
+
+            Console.WriteLine("A média da soma de todos os números do vetor é: {0}", media);
+        }
+        static int somaValoresMatriz(int[,] numero)
+        {
+            int somaTotal = 0;
+            for (int i = 0; i < numero.GetLength(0); i++)
+            {
+                for (int j = 0; j < numero.GetLength(1); j++)
+                {
+                    somaTotal += numero[i, j];
+                }
+            }
+            return somaTotal;
+        }
+    }
+}
